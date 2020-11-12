1051f61b428d4b965288df70f5ba105c66b4c201
Vinicius Paulino <vinicius.paulino@softplan.com.br>
2020-11-11 21:56:05 -0300

    create model of appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-    id: string;
-    provider: string;
-    date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -25,11 +19,7 @@ appointmentsRouter.post('/', (request, response) => {
             .status(400)
             .json({ message: 'This appointment is already booked' });
     }
-    const appointment = {
-        id: uuid(),
-        provider,
-        date: parsedDate,
-    };
+    const appointment = new Appointment(provider, parsedDate);
 
     appointments.push(appointment);
 
