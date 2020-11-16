8d46dc5a5b09255155ca4d786b3c7c9d742bb7d5
Vinicius Paulino <vinicius.paulino@softplan.com.br>
2020-11-15 22:57:45 -0300

    model and migration users

--- src/database/migrations/1605488873121-CreateUsers.ts
+++ src/database/migrations/1605488873121-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1605308886340
-    implements MigrationInterface {
+export default class CreateUsers1605488873121 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -15,12 +14,18 @@ export default class CreateAppointments1605308886340
                         default: 'uuid_generate_v4()',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
+                        isNullable: false,
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
                         name: 'created_at',
@@ -38,6 +43,6 @@ export default class CreateAppointments1605308886340
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 }
