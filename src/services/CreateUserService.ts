3e4a3382fdafa6d07e23c919d211227d66de98fd
Vinicius Paulino <vinicius.paulino@softplan.com.br>
2020-11-19 18:12:58 -0300

    using class error

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 import User from '../models/User';
 
+import AppError from '../errors/AppError';
+
 interface Request {
     name: string;
     email: string;
@@ -17,7 +19,7 @@ class CreateUserService {
         });
 
         if (checkUserExiste) {
-            throw new Error('Email address already used.');
+            throw new AppError('Email address already used.');
         }
 
         const hashedPassoword = await hash(password, 8);
