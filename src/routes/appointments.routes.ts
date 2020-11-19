7f1beacc7accad53b7fcbc3900fff73ffbeca42f
Vinicius Paulino <vinicius.paulino@softplan.com.br>
2020-11-19 19:19:04 -0300

    create global exception handler

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -19,21 +19,17 @@ appointmentsRouter.get('/', async (_, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-    try {
-        const { provider_id, date } = request.body;
+    const { provider_id, date } = request.body;
 
-        const parsedDate = parseISO(date);
+    const parsedDate = parseISO(date);
 
-        const createAppointment = new CreateAppointmentService();
+    const createAppointment = new CreateAppointmentService();
 
-        const appointment = await createAppointment.execute({
-            date: parsedDate,
-            provider_id,
-        });
-        return response.json(appointment);
-    } catch (error) {
-        return response.status(400).json({ error: error.message });
-    }
+    const appointment = await createAppointment.execute({
+        date: parsedDate,
+        provider_id,
+    });
+    return response.json(appointment);
 });
 
 export default appointmentsRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -33,20 +33,16 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        try {
-            const updateUserAvatar = new UpdateUserAvatarService();
+        const updateUserAvatar = new UpdateUserAvatarService();
 
-            const user = await updateUserAvatar.execute({
-                user_id: request.user.id,
-                avatarFileName: request.file.filename,
-            });
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFileName: request.file.filename,
+        });
 
-            delete user.password;
+        delete user.password;
 
-            return response.json(user);
-        } catch (error) {
-            return response.status(400).json({ error: error.message });
-        }
+        return response.json(user);
     },
 );
 export default usersRouter;
