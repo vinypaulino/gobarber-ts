edf906b012f57af01022293e0a1dcfd4868802bc
Vinicius Paulino <vinicius.paulino@softplan.com.br>
2020-11-21 08:53:44 -0300

    config imports

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,8 +1,9 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
-import User from '../entities/User';
 
-import AppError from '../../../shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
+
+import User from '../infra/typeorm/entities/User';
 
 interface Request {
     name: string;
